8be7ad4122af728246718eeb48f9853ddfe0848d
Adrian Wong <adrianw1832@hotmail.co.uk>
2015-09-14 16:15:50 +0100

    before updating from upstream

--- app.js
+++ app.js
@@ -6,8 +6,8 @@ var W = 500;
 
 var x = 45;
 var y = 200;
-var dx = 5;
-var dy = 5;
+var dx = 1;
+var dy = 1;
 
 canvas.height = H;
 canvas.width = W;
@@ -18,12 +18,34 @@ function draw() {
   ctx.arc(x, y, 15, 0, Math.PI*2, true);
   ctx.closePath();
   ctx.fill();
-  if(x > 485 || x < 15) { dx = -dx} ;
-  if(y > 485 || y < 15) { dy = -dy };
+  if(x > 485 || x < 15) { dx = -dx; }
+  if(y > 485 || y < 15) { dy = -dy; }
   x += dx;
   y += dy;
 }
 
+function onClick(e) {
+  var element = canvas;
+  var offsetX = 0, offsetY = 0;
+
+  if (element.offsetParent) {
+    do {
+      offsetX += element.offsetLeft;
+      offsetY += element.offsetTop;
+    } while ((element = element.offsetParent));
+  }
+
+  mouseX = e.pageX - offsetX;
+  mouseY = e.pageY - offsetY;
+  determineNewDirection(mouseX, mouseY);
+}
+
+function determineNewDirection(mouseX, mouseY) {
+  if (x > mouseX) dx = -dx;
+  if (y > mouseX) dy = -dy;
+}
+
 function init(){
   setInterval(draw, 30);
+  canvas.addEventListener("click", onClick, false);
 }
